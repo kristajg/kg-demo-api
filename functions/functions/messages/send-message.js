60f4196752d93aba9b424de0f072a036cf873e50
Brandon Corn <cornbrandonk@gmail.com>
2022-11-26 11:08:31 -0700

    remove console log test

--- functions/functions/messages/send-message.js
+++ functions/functions/messages/send-message.js
@@ -2,7 +2,6 @@ const { sendMessage } = require(Runtime.getFunctions()["twilio/messages"].path);
 const Response = require(Runtime.getFunctions()["util/response"].path);
 
 exports.handler = (context, event, callback) => {
-    console.log("we called the function");
     const client = context.getTwilioClient();
     const { messageBody, toNumber, fromNumber } = event;
     const from = fromNumber || context.MY_PHONE_NUMBER;
